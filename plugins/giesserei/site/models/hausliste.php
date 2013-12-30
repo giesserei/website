264f434c754e41071f40a16731b73b56e778f8d2
Steffen Förster <github96758@4foerster.ch>
2013-12-31 00:34:05 +0100

    - Nur Mitglieder berücksichtigen, denen eine Wohnung zugewiesen ist - Auch bei der letzten Wohnung vom Haus 8 werden die Kinder angezeigt - Wohnungen ohne Bewohner werden in der Bewohnerliste nicht angezeigt - Tabellen-Zelle zu jeder Wohnung mit End-Tags abschliessen

--- plugins/giesserei/site/models/hausliste.php
+++ plugins/giesserei/site/models/hausliste.php
@@ -3,18 +3,29 @@ defined('_JEXEC') or die('Restricted access');
 
 jimport('joomla.application.component.model');
 
+/**
+ * Modell-Klasse für die Seite "Bewohnerliste".
+ * 
+ * Changes:
+ * - Nur Mitglieder berücksichtigen, denen eine Wohnung zugewiesen ist (SF, 2013-12-31)
+ * - Wohnungen ohne Bewohner werden in der Bewohnerliste nicht gezeigt (SF, 2013-12-31)
+ * 
+ * @author JAL
+ */
 class GiessereiModelHausliste extends JModel {
-
-	// Erzeugt eine Liste mit allen bewohnten Einheiten und deren Mitgliedern
-	function getBelegung( ) {
-	    $db =& JFactory::getDBO();
-	    $query = "SELECT * FROM #__mgh_mitglied as mgl
-	    		LEFT JOIN #__mgh_x_mitglied_mietobjekt AS xmo ON mgl.userid = xmo.userid 
-				ORDER BY objektid";
-	    $db->setQuery($query);
-	    $rows = $db->loadObjectList();
-	    return($rows);
-	} // end getReservation
-	
+  
+  /**
+   * Erzeugt eine Liste mit allen bewohnten Einheiten und deren Bewohnern.
+   * Sind einem Bewohner mehrere Wohnungen zugewiesen, so ist dieser Bewohner mehrfach in der Liste enthalten.
+   */ 
+  function getBelegung() {
+    $db = & JFactory::getDBO();
+    $query = "SELECT * FROM #__mgh_mitglied as mgl
+	    		    JOIN #__mgh_x_mitglied_mietobjekt AS xmo ON mgl.userid = xmo.userid 
+				      ORDER BY objektid";
+    $db->setQuery($query);
+    $rows = $db->loadObjectList();
+    return ($rows);
+  }
 }
 ?>
--- plugins/giesserei/site/views/hausliste/tmpl/default.php
+++ plugins/giesserei/site/views/hausliste/tmpl/default.php
@@ -1,10 +1,14 @@
 <?php
-/*
- * com_mitglied:View Hausliste
- * Zeigt eine Liste der Häuser mit ihren BewohnerInnen an
+/**
+ * Zeigt eine Liste der Häuser mit ihren BewohnerInnen an.
  * 
- * Jürg Altwegg, Hausverein Giesserei
+ * Changes:
+ * - Nur Mitglieder berücksichtigen, denen eine Wohnung zugewiesen ist (SF, 2013-12-31)
+ * - Auch bei der letzten Wohnung vom Haus 8 werden die Kinder angezeigt (SF, 2013-12-31)
+ * - Wohnungen ohne Bewohner werden in der Bewohnerliste nicht angezeigt (SF, 2013-12-31)
+ * - Tabellen-Zelle zu jeder Wohnung mit End-Tags abschliessen (SF, 2013-12-31)
  * 
+ * @author Jürg Altwegg
  */
 
 defined('_JEXEC') or die('Restricted access');
@@ -15,15 +19,16 @@ $base = JURI::base(true);
 $doc->addStyleSheet($base.'/components/com_giesserei/template/giesserei_default.css');
 
 require_once(JPATH_SITE.DS."components".DS."com_giesserei".DS."models".DS."mitgliederliste.php");
-$listen_model = new GiessereiModelMitgliederliste; // Referenz auf Modell für Methoden des Objekts
 
+// Referenz auf Modell der Mitgliederliste
+$listen_model = new GiessereiModelMitgliederliste; 
 
 // Benutzer angemeldet?
 $user =& JFactory::getUser();
-if(!$user->id > 0):
+if (!$user->id > 0) {
 	echo "Die Registrierung ist abglaufen. Bitte neu anmelden.";
 	exit();
-endif;
+}
 
 // AJAX-Tools laden
 JHTML::_('behavior.mootools');
@@ -31,7 +36,12 @@ JHTML::_('behavior.modal');
 
 ?>
 
-<p style="border-style:solid; border-color:red; border-width:1px; padding:5px;">Diese Mitgliederliste darf nur für <strong>persönliche</strong> Zwecke verwendet werden. Die Nutzung für <strong>Sammelversände, Umfragen, Newsletter und ähnliches ist explizit dem Vorstand des Vereins Mehrgenerationenhaus vorbehalten</strong>. Jeglicher Missbrauch wird zivilrechtlich geahndet.</p>
+<p style="border-style:solid; border-color:red; border-width:1px; padding:5px;">
+  Diese Mitgliederliste darf nur für <strong>persönliche</strong> Zwecke verwendet werden. 
+  Die Nutzung für <strong>Sammelversände, Umfragen, Newsletter und ähnliches ist explizit 
+  dem Vorstand des Vereins Mehrgenerationenhaus vorbehalten</strong>. 
+  Jeglicher Missbrauch wird zivilrechtlich geahndet.
+</p>
 
 <p><em>Um die Details eines Mitglieds anzuzeigen, auf den Namen klicken!</em></p>
 
@@ -39,54 +49,103 @@ JHTML::_('behavior.modal');
 
 <?php
 
-$vorgaenger = "";
-$haus = array(1 => '<h2>Haus 1</h4><table class="mitgliederliste">', 2 => '<h2>Haus 2</h4><table class="mitgliederliste">',
-	3 => '<h2>Haus 3</h4><table class="mitgliederliste">', 4 => '<h2>Haus 4</h4><table class="mitgliederliste">', 5 => '<h2>Haus 5</h4><table class="mitgliederliste">', 
-	6 => '<h2>Haus 6</h4><table class="mitgliederliste">',7 => '<h2>Haus 7</h4><table class="mitgliederliste">',8 => '<h2>Haus 8</h4><table class="mitgliederliste">');
+$haus = array(
+    1 => '<h2>Haus 1</h2><table class="mitgliederliste">', 
+    2 => '<h2>Haus 2</h2><table class="mitgliederliste">',
+	  3 => '<h2>Haus 3</h2><table class="mitgliederliste">', 
+    4 => '<h2>Haus 4</h2><table class="mitgliederliste">', 
+    5 => '<h2>Haus 5</h2><table class="mitgliederliste">', 
+	  6 => '<h2>Haus 6</h2><table class="mitgliederliste">',
+    7 => '<h2>Haus 7</h2><table class="mitgliederliste">',
+    8 => '<h2>Haus 8</h2><table class="mitgliederliste">');
 
-foreach($this->objekte as $obj):
+/**
+ * Sofern unter der übergebenen Objekt-Id Kinder registriert sind, wird 
+ * ein HTML-Schnipsel mit den Namen der Kinder zurückgegeben.
+ */
+function getKinderHtml($objektid, $model) {
+  $result = "";
+  $kinder = $model->getKinder($objektid);
+  
+  // Anzahl der Kinder ermitteln, für eine Anzeige-Freigabe besteht
+  $anzahlMitFreigabe = 0;
+  if (count($kinder) > 0) {
+    foreach ($kinder as $kind) {
+      if ($kind->name_frei) {
+        $anzahlMitFreigabe ++;
+      }
+    }
+  }
+  
+  if ($anzahlMitFreigabe > 0) {
+    $result .= ($anzahlMitFreigabe > 1) ? "Mit Kindern: " : "Mit Kind: ";
 
-	
-	if($vorgaenger != $obj->objektid):
-		// Kinder auflisten
-		$kinder = $listen_model->getKinder($vorgaenger);
-	
-		// Ist eines der Kinder zur Anzeige freigegeben?
-		$freigabe = false;
-		if(count($kinder) > 0) foreach($kinder as $kind):
-			if($kind->name_frei) $freigabe = true;
-		endforeach;
-		
-		if(count($kinder) > 0 AND $freigabe):
-			if(count($kinder) > 1):
-				$haus[substr($vorgaenger,2,1)] .= "Mit Kindern: ";
-			else:
-				$haus[substr($vorgaenger,2,1)] .= "Mit Kind: ";
-			endif;
-			foreach ($kinder as $kind):
-				if($kind->name_frei) $haus[substr($vorgaenger,2,1)] .= $kind->vorname;
-				if($kind->jahrgang_frei) $haus[substr($vorgaenger,2,1)] .= " (".$kind->jahrgang.")";
-				if($kind->handy_frei) $haus[substr($vorgaenger,2,1)] .= " ".$kind->handy." ";
-				if($kind != end($kinder)) $haus[substr($vorgaenger,2,1)] .= ", ";
-			endforeach;
-			$haus[substr($vorgaenger,2,1)] .= "<br />\n";
-		endif;
-	
-		$haus[substr($obj->objektid,2,1)] .= "<tr><td class=\"mitglied\">".$obj->objektid.": </td><td class=\"mitglied\">";
-	endif;
-	
-	$haus[substr($obj->objektid,2,1)] .= "<a class=\"modal\"
-	href=\"index.php?option=com_giesserei&controller=giesserei&view=mitglied&tmpl=component&id=".$obj->userid."\"
-	rel=\"{handler: 'iframe', size: {x: 640, y: 480}}\">".$obj->vorname." ".$obj->nachname."</a><br />\n";
-	
-	$vorgaenger = $obj->objektid;
-	
-endforeach;
+    $current = 0;
+    foreach ($kinder as $kind) {
+      if ($kind->name_frei) {
+        $current ++;
+        $result .= $kind->vorname;
+        
+        if ($kind->jahrgang_frei) {
+          $result .= " (".$kind->jahrgang.")";
+        }
+        if ($kind->handy_frei) {
+          $result .= " ".$kind->handy." ";
+        }
+      }
+      
+      // weitere Kinder vorhanden?
+      if ($current < $anzahlMitFreigabe) {
+        $result .= ", ";
+      }
+    }
+    $result .= "<br />\n";
+  }
+  return $result;
+}
 
-foreach ($haus as $hs):
+/**
+ * Liefert die Hausnummer zur übergebenen Wohnungsnummer.
+ */
+function getHausNr($objektid) {
+  return substr($objektid,2,1);
+}
+
+$vorgaengerId = "";
+
+foreach($this->objekte as $obj) {
+  $hausNr = getHausNr($obj->objektid);
+  
+  // Wohnung wechselt und ist nicht die erste Wohnung der Liste 
+  // -> Kinder ausgeben
+  // -> Tabellenzeile abschliessen
+  if($vorgaengerId != $obj->objektid && $vorgaengerId != "") {
+    $hausNrVorgaenger = getHausNr($vorgaengerId);
+    $haus[$hausNrVorgaenger] .= getKinderHtml($vorgaengerId, $listen_model);
+    $haus[$hausNrVorgaenger] .= "</td></tr>";
+  }
+  
+  // Wohnung wechselt oder erste Wohnung -> neue Tabellenzeile 
+	if($vorgaengerId != $obj->objektid) {
+		$haus[$hausNr] .= "<tr><td class=\"mitglied\">" . $obj->objektid . ": </td><td class=\"mitglied\">";
+	}
+	
+	// Einen Bewohner mit Vornamen und Namen ausgeben
+	$haus[$hausNr] .= "<a class=\"modal\"
+	  href=\"index.php?option=com_giesserei&controller=giesserei&view=mitglied&tmpl=component&id=".$obj->userid."\"
+	  rel=\"{handler: 'iframe', size: {x: 640, y: 480}}\">".$obj->vorname." ".$obj->nachname."</a><br />\n";
+	
+	$vorgaengerId = $obj->objektid;
+}
+
+// Für die letzte Wohnung die Kinder ausgeben und Tabellenzeile abschliessen
+$hausNrVorgaenger = getHausNr($vorgaengerId);
+$haus[$hausNrVorgaenger] .= getKinderHtml($obj->objektid, $listen_model);
+$haus[$hausNrVorgaenger] .= "</td></tr>";
+
+foreach ($haus as $key=>$hs) {
 	echo $hs."</table><br />";
-endforeach;
-
+}
 
 ?>
 </table>
