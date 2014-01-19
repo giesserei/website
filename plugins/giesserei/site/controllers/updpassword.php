636aa3b5f9c2cd10ce7e30b23980ce2b43a45c39
Steffen Förster <github96758@4foerster.ch>
2014-01-19 18:18:41 +0100

    Basisklasse eingeführt

--- plugins/giesserei/site/controllers/updpassword.php
+++ plugins/giesserei/site/controllers/updpassword.php
@@ -3,6 +3,7 @@ defined('_JEXEC') or die('Restricted access');
 
 JLoader::register('GiessereiFrontendHelper', JPATH_COMPONENT . '/helpers/giesserei_frontend.php');
 JLoader::register('GiessereiConst', JPATH_COMPONENT . '/helpers/giesserei_const.php');
+JLoader::register('GiessereiControllerUpdBase', JPATH_COMPONENT . '/controllers/upd_base.php');
 
 jimport('joomla.application.component.controllerform');
 
@@ -11,152 +12,35 @@ jimport('joomla.application.component.controllerform');
  *
  * @author Steffen Förster
  */
-class GiessereiControllerUpdpassword extends JControllerForm {
+class GiessereiControllerUpdpassword extends GiessereiControllerUpdBase {
+  
+  // -------------------------------------------------------------------------
+  // protected section
+  // -------------------------------------------------------------------------
   
   /**
-   * Führt nach ein paar Vorarbeiten einen Redirect auf die View durch, welche das Passwort-Formular anzeigt.
+   * @see GiessereiControllerUpdBase::getViewName()
    */
-  public function edit() {
-    GiessereiFrontendHelper::methodBegin('GiessereiControllerUpdpassword', 'edit');
-    
-    if (!GiessereiFrontendHelper::checkAuth()) {
-      return false;
-    }
-    
-    $this->redirectEditView();
-    
-    return true;
+  protected function getViewName() {
+    return "updpassword";
   }
   
   /**
-   * Speichert das neue Passwort in der Datenbank.
+   * @see GiessereiControllerUpdBase::saveDataInSession()
    */
-  public function save() {
-    GiessereiFrontendHelper::methodBegin('GiessereiControllerUpdpassword', 'save');
-    
-    $app = JFactory::getApplication();
-    
-    // Form-Token prüfen -> Token wird in Template gesetzt
-    JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
-    
-    $formData = $this->getFormData();
-
-    // Validierung -> Validierungsmeldungen werden direkt ausgegeben
-    if (!$this->validateData($formData)) {
-      return false;
-    }
-    
-    // Daten Speichern
-    if ($this->processSave($formData)) {
-      $this->redirectProfilView();
-      return true;
-    }
-    
+  protected function saveDataInSession() {
     return false;
   }
   
-  // -------------------------------------------------------------------------
-  // private section
-  // -------------------------------------------------------------------------
-  
   /**
-   * Holt die Formulardaten aus dem JInput. Es sind nur die Passwortfelder zulässig.
+   * @see GiessereiControllerUpdBase::filterFormFields()
    */
-  private function getFormData() {
-    $app = JFactory::getApplication();
-    $input = $app->input;
-    $model = $this->getModel();
-    $form = $model->getForm(array(), false);
-    $data = $input->get($form->getFormControl(), '', 'array');
-    
+  protected function filterFormFields($data) {
     $dataAllowed = array();
     $dataAllowed['password'] = $data['password'];
     $dataAllowed['password2'] = $data['password2'];
-    
+  
     return $dataAllowed;
-  }
-  
-  /**
-   * Prüft, ob die Eingaben korrekt sind. Validierungsmeldungen werden direkt ausgegeben.
-   * 
-   * @return boolean True, wenn alle Eingaben korrekt sind.
-   */
-  private function validateData($data) {
-    $app = JFactory::getApplication();
-    $model = $this->getModel();
-    $form = $model->getForm($data, false);
-    
-    $result = $model->validate($form, $data);
-    
-    // Nur die ersten drei Fehler dem Benutzer anzeigen
-    if ($result === false) {
-      $errors = $model->getErrors();
-    
-      for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++) {
-        if ($errors[$i] instanceof Exception) {
-          $app->enqueueMessage($errors[$i]->getMessage(), 'warning');
-        }
-        else {
-          $app->enqueueMessage($errors[$i], 'warning');
-        }
-      }
-    
-      // Zurück zum Formular
-      $this->redirectEditView();
-    
-      return false;
-    }
-    
-    return true;
-  }
-  
-  /**
-   * Speichert das neue Passwort. Der Code entstammt teilweise vom Kunena-Plugin.
-   * 
-   * Fehlermeldungen werden direkt angezeigt.
-   *
-   * @return boolean True, wenn das Speichern erfolgreich war
-   */
-  private function processSave($data) {
-    $app = JFactory::getApplication();
-    $model = $this->getModel();
-  
-    // Fehlermeldung dem Benutzer anzeigen
-    if (!$model->save($data)) {
-      $errors = $model->getErrors();
-      foreach ($errors as $error) {
-        $app->enqueueMessage($error, 'warning');
-      }
-  
-      // Zurück zum Formular
-      $this->redirectEditView();
-  
-      return false;
-    }
-  
-    return true;
-  }
-  
-  /**
-   * Auf die Edit-View weiterleiten. Es wird auch die Menü-Id der Profilseite gesetzt, damit das Menü sich nicht verstellt.
-   */
-  private function redirectEditView() {
-    $app = JFactory::getApplication();
-    $menuId = $app->getUserState(GiessereiConst::SESSION_KEY_PROFIL_MENU_ID);
-    $this->setRedirect(
-        JRoute::_('index.php?option=com_giesserei&view=updpassword&layout=edit&Itemid=' . $menuId, false)
-    );
-  }
-  
-  /**
-   * Auf die Profilansicht weiterleiten.
-   */
-  private function redirectProfilView() {
-    $app = JFactory::getApplication();
-    $menuId = $app->getUserState(GiessereiConst::SESSION_KEY_PROFIL_MENU_ID);
-    $this->setRedirect(
-        JRoute::_('index.php?option=com_giesserei&view=profil&layout=view', false)
-    );
   }
   
 }
