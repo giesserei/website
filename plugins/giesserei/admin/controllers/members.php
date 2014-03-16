edef1a71a2210ebe8fb44c50522bd5157f635b83
Steffen Förster <github96758@4foerster.ch>
2014-03-16 21:32:25 +0100

    Kommentar

--- plugins/giesserei/admin/controllers/members.php
+++ plugins/giesserei/admin/controllers/members.php
@@ -1,8 +1,4 @@
 <?php
-/*
- * Created on 27.12.2010
- *
- */
 
 defined('_JEXEC') or die('Restricted access');
 
@@ -10,22 +6,13 @@ jimport ('joomla.application.component.controlleradmin');
 
 class GiessereiControllerMembers extends JControllerAdmin {
 
-  /**
-   * Verbindung zu MyThingsModelMyThing, damit die dort
-   * enthaltenen Methoden zum Lesen von Datensätzen
-   * verwendet werden können.
-   *
-   * @return MyThingsModelMyThings Das Model für die Listenansicht
-   */
   public function getModel($name = 'Member', $prefix = 'GiessereiModel', $config = array())
   {
     // Model nicht automatisch mit Inhalten aus dem Request befüllen
     $config['ignore_request'] = true;
 
-    // restliche Arbeit der Elternklasse überlassen
     return parent::getModel($name, $prefix, $config);
   }
-
 
 }
 
