c8d8c4b643268dfad583ae59aa157ffb9a12ab90
Steffen Förster <github96758@4foerster.ch>
2014-03-16 21:35:12 +0100

    Anpassung an neue Berechtigungen

--- plugins/giesserei/admin/controller.php
+++ plugins/giesserei/admin/controller.php
@@ -5,43 +5,120 @@ jimport('joomla.application.component.controller');
 
 JLoader::register('GiessereiHelper', JPATH_COMPONENT . '/helpers/giesserei.php');
 
-class GiessereiController extends JController
-{
-	/**
-	 * @var string Standardview
-	 */
-	protected $default_view = 'members';
+class GiessereiController extends JController {
 
 	/**
-	 * Ausgabe der View members.
-	 * @inherit
+	 * Wenn keine View gewählt wurde, wird die View "welcome" gezeigt.
+	 *  
+	 * @see JController::display()
 	 */
 	public function display($cachable = false, $urlparams = false)
 	{
-		/* @var $input JInput Unsere Einnahmequelle */
 		$input = JFactory::getApplication()->input;
 
 		// alle Variablen mit Vorgabewerten initialisieren
-		$view   = $input->get('view', $this->default_view);
+		$view   = $input->get('view', "empty");
 		$layout = $input->get('layout', 'default');
+		$task   = $input->get('task', 'default');
 		$id     = $input->get('id');
 
-		// Bevor die View aufgebaut wird, erstellt die Helperklasse
-		// ein Untermenü zum Wechseln zwischen Categories und Things
-		GiessereiHelper::addSubmenu($view);
-
-		if ($view == 'members' && $layout == 'edit')
-		{
-			// checkEditId() ist eine Methode von JController, die den Kontext prüft
-			if (!$this->checkEditId('com_giesserei.edit.member', $id)) {
-				// Kommentarlos zurück zur default-view
-				$this->setRedirect(JRoute::_('index.php?option=com_giesserei&view=members', false));
-				return false;
-			}
+		if ($view === 'empty') {
+		  $this->setRedirect(JRoute::_('index.php?option=com_giesserei&view=welcome', false));
+		  return false;
 		}
+		
+		// Zugriffe prüfen
+		if (! $this->isAuthorised($view, $task)) {
+		  return false;
+		}
+		
+		// Submenü erstellen
+		$this->addSubmenu($view);
 
 		// Alles geprüft und ok, die View kann ausgegeben werden
 		parent::display($cachable, $urlparams);	
+	}
+	
+	// -------------------------------------------------------------------------
+	// private section
+	// -------------------------------------------------------------------------
+	
+	private function isAuthorised($view, $task) {
+	  $user = JFactory::getUser();
+	  $assetname = 'com_giesserei';
+	  
+	  if ($view === 'members') {
+	    return $user->authorise('view.member', $assetname);
+	  }
+	  if (GiessereiHelper::startsWith($task, 'member')) {
+	    return $user->authorise('edit.member', $assetname) || $user->authorise('edit.member.vkom', $assetname);
+	  }
+	  if ($view === 'kids') {
+	    return $user->authorise('view.kid', $assetname);
+	  }
+	  if (GiessereiHelper::startsWith($task, 'kid')) {
+	    return $user->authorise('edit.kid', $assetname);
+	  }
+	  if ($view === 'flats') {
+	    return $user->authorise('view.flat', $assetname);
+	  }
+	  if (GiessereiHelper::startsWith($task, 'flat')) {
+	    return $user->authorise('edit.flat', $assetname);
+	  }
+	  
+	  return true;
+	}
+	
+	/**
+	 * In der Listenansicht im Contentbereich ein Submenü aufbauen. Hierbei werden die vorhandenen Berechtigungen 
+	 * berücksichtigt.
+	 */
+	private function addSubmenu($name) {
+	  $user = JFactory::getUser();
+	  $assetname = 'com_giesserei';
+	  
+    JSubMenuHelper::addEntry(
+      JText::_('Start'),
+      'index.php?option=com_giesserei&view=welcome', $name == 'welcome'
+    );
+	  
+	  if ($user->authorise('view.member', $assetname)) {
+	    JSubMenuHelper::addEntry(
+	      JText::_('Mitgliederliste'),
+	      'index.php?option=com_giesserei&view=members', $name == 'members'
+	    );
+	  }
+	
+	  if ($user->authorise('view.kid', $assetname)) {
+	    JSubMenuHelper::addEntry(
+	      JText::_('Kinder'),
+	      'index.php?option=com_giesserei&view=kids', $name == 'kids'
+	    );
+	  }
+	
+	  if ($user->authorise('view.flat', $assetname)) {
+	    JSubMenuHelper::addEntry(
+	      JText::_('Wohnungen'),
+	      'index.php?option=com_giesserei&view=flats', $name == 'flats'
+	    );
+	  }
+	
+	  // nutzen wir zunächst nicht -> Einsatz ist nicht geklärt
+	  /*
+	  JSubMenuHelper::addEntry(
+	    JText::_('Mitglieder-Journal-Klassen'),
+	    'index.php?option=com_giesserei&view=mjournalclasses', $name == 'mjournalclasses'
+	  );
+	  */
+	
+	  // nutzen wir zunächst nicht -> Einsatz ist nicht geklärt
+	  /*
+	  JSubMenuHelper::addEntry(
+	    JText::_('Wohnungs-Journal-Klassen'),
+	    'index.php?option=com_giesserei&view=ojournalclasses', $name == 'ojournalclasses'
+	  );
+	  */
+	
 	}
 }
 ?>
